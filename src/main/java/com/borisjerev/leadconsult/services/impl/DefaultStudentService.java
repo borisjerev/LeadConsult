948643ae82f4dd13f0a9db40b2e20b6070eb593e
borisjerev <borisjerev@users.noreply.github.com>
2022-10-17 21:59:48 +0300

    remove unncesary imports

--- src/main/java/com/borisjerev/leadconsult/services/impl/DefaultStudentService.java
+++ src/main/java/com/borisjerev/leadconsult/services/impl/DefaultStudentService.java
@@ -9,7 +9,6 @@ import com.borisjerev.leadconsult.services.contract.StudentService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
